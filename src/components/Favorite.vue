e6b089709cfe77f8e3f76f7cea1d0c9a63b9bcf3
kaingsak sakpreeda <shirof.white@gmail.com>
2019-05-08 22:45:12 +0700

    ทำหน้า favแบบกากๆ

--- src/components/Favorite.vue
+++ src/components/Favorite.vue
@@ -5,15 +5,59 @@
         Favorites
     </p>
     <div class= "ui segment">
-      
+       <div class="ui grid">
+          <div
+            class="eight wide column"
+            id="pic"
+            v-for="auser in Users"
+            v-bind:key="auser.id"
+          >
+            <div class="ui centered link cards">
+                <div class="image">
+                  <img style="height: 60%;" v-bind:src="'http://api.adorable.io/avatars/285/'+auser.firstName+'.png'">
+              </div>
+              <div class="ui button">cccc</div>
+            </div>
+          </div>
+        </div>
   </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 /* eslint-disable */
   export default {
-    name: 'favorite'
+    name: 'favorite',
+  data(){
+    return{
+      Users:[],
+      Pets:[]
+    }
+  },
+  methods(){
+
+  },
+  mounted(){
+      var url = "http://localhost:3000/user"
+    axios
+      .get(url)
+      .then(response => {
+        console.log(response.data);
+        this.Users = response.data;
+        console.log(this.Users)
+        this.Pets = this.Users.map((user)=>{
+          console.log(user.pet)
+          return user.pet
+        });
+        console.log(response.data[0].pet)
+        console.log(response.data[0].pet.pictures[0].large)
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    }
+    
   }
 </script>
 
