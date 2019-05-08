36bb876fcde7a77cd15afbd823b9d717531b3ebd
Unknown <toomanyumm@gmail.com>
2019-05-08 18:42:39 +0700

    Profile ครั้งที่1 หน้า edit MainProfile setting

---
+++ src/components/MainProfile.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="ui raised very padded text container segment">
+    <div>
+        <div class="ui  very  container centered aligned">
+    <img class="ui medium circular image centered" src=" https://semantic-ui.com/images/avatar/large/stevie.jpg">
+        </div>
+     <div class="meta">
+        <a>Friends</a>
+      </div>
+      <div class="description">
+        Matthew is an interior designer living in New York.
+      
+    </div>
+    <div class="extra content">
+      <span class="right floated">
+        Joined in 2013
+      </span>
+      <span>
+        <i class="user icon"></i>
+        75 Friends
+      </span>
+    </div>
+    <!--<hr style="width: 5px; height: 100px; background: black; border: none;" />-->
+    <div class="ui large buttons">
+  <button class="ui button" onclick="location.href='#/setting'" > <img src="https://s3.amazonaws.com/peoplepng/wp-content/uploads/2019/01/12074518/logo-setting-png-4.png" width="50px" height="50px"></button>
+  <div class="or"></div>
+  <button class="ui button" onclick="location.href='#/edit'"><img src="http://freevector.co/wp-content/uploads/2011/12/84380-edit-button.png" width="50px" height="50px" alt=""></button>
+</div>
+    </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+.divformain {
+  border-radius: 50%;
+}
+</style>
---
+++ src/components/setting.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="ui raised very padded text container segment">
+        <div class="vertical-menu segment">
+        <a href="#" class="active">Home</a>
+        <a href="#">Phone Number</a>
+        <a href="#">Email</a>
+        <a href="#">Location</a>
+        <a href="#">
+            <div class="slidecontainer">Maximum Distance (KM)
+                <form oninput="result.value=parseInt(b.value)">
+                    <input type="range" min="1" max="100" value="" class="slider" id="b" >
+                    <output name="result"></output>
+                </form>
+            </div>
+                    </a>
+        <a href="#">Gender</a>
+        <a href="#"><output id="myRange" ></output></a>
+        </div>
+    </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style>
+.vertical-menu {
+  width: auto px; /* Set a width if you like */
+}
+
+.vertical-menu a {
+  background-color: #eee; /* Grey background color */
+  color: black; /* Black text color */
+  display: block; /* Make the links appear below each other */
+  padding: 12px; /* Add some padding */
+  width: auto px;
+  text-decoration: none; /* Remove underline from links */
+}
+
+.vertical-menu a:hover {
+  background-color: #ccc; /* Dark grey background on mouse-over */
+}
+
+.vertical-menu a.active {
+  background-color: orange; /* Add a green color to the "active/current" link */
+  color: white;
+}
+
+.slidecontainer {
+  width: 100%; /* Width of the outside container */
+}
+
+.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 15px;
+  border-radius: 5px;
+  background: orange;
+  outline: none;
+  opacity: 0.7;
+  -webkit-transition: 0.2s;
+  transition: opacity 0.2s;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: orangered;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #4caf50;
+  cursor: pointer;
+}
+</style>
+
